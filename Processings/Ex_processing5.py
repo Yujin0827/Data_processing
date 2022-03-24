eca80ef4729087eac582d122c657ed46c8526511
Yujin0827 <yjjeon980827@gmail.com>
2022-03-24 12:41:15 +0900

    결과 파일 추출 오류 해결

--- Processings/Ex_processing5.py
+++ Processings/Ex_processing5.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 
 >>> 폴더 내 여러 환자의 데이터 -> 각 tsv로 정리 
@@ -13,7 +14,7 @@ from xml.etree.ElementTree import parse
 import pandas as pd
 import os
 
-def get_columns(input_path):
+def get_columns(input_path, patient_id, patient_uid):
 
     is_catch_up_saccade_analysis = False
     is_impulse = False
@@ -24,12 +25,11 @@ def get_columns(input_path):
     columns = []
     col_max = 0
     
-    print(input_path)
     
     with open(input_path, 'r', encoding='UTF-8') as fin:
         for line in fin:
-            print(line)
             tokens = line.split(',')
+            
 
             if 'Patient Name:' in line:
                 patient_name = ' '.join(tokens[1:])
@@ -139,173 +139,190 @@ def get_columns(input_path):
 
         return []
 
+def parse_csv(file):
+    if '.xml' in file:
+        xml_path = os.path.join(path, file).replace('\\', '/')
+        input_path = xml_path.strip('.xml') + '.csv'
+        result_path = path.replace('vHIT', 'vHIT_Result') + '/' + file.strip('.xml') + '.tsv'
+        result_dir = os.path.dirname(result_path)
+        
+        if not os.path.exists(result_dir):
+            os.makedirs(result_dir)
+            
+        
+        is_catch_up_saccade_analysis = False
+        is_impulse = False
+        
+        meta_dict = {}
+        impulse_dict = {}
+        all_dict = {}
+        all_dict_list = []
+        
+        s_group = ''
+        impulse_key = ''
+        impulse_num = 1
+        
+        
+        xml_doc = ET.parse(xml_path)
+        root = xml_doc.getroot()
+        
+        ics_patient = root.findall("{http://tempuri.org/PMRExportDataSet.xsd}ICSPatient")
+        patient_id = [x.findtext("{http://tempuri.org/PMRExportDataSet.xsd}PatientID") for x in ics_patient][0]
+        patient_uid = [x.findtext("{http://tempuri.org/PMRExportDataSet.xsd}PatientUID") for x in ics_patient][0]
+        patient_name = ""
+        
+        columns = get_columns(input_path, patient_id, patient_uid)
+        # print("\t".join(columns))
+        
+        
+        with open(input_path, 'r', encoding='UTF-8') as fin:
+            for line in fin:
+                line = line.strip()
+                
+                tokens = line.split(',')
+                
+                
+                if 'Patient Name:' in line:
+                    patient_name = ' '.join(tokens[1:]).strip()
+                    
+                    
+                elif 'Test Date' in line:
+                    if len(meta_dict) != 0:
+                        for col in columns:
+                            if col in meta_dict:
+                                # print(meta_dict[col], end='\t')
+                                all_dict[col] = meta_dict[col]
+        
+                            for impulse_key, impulse_value in impulse_dict.items():
+                                for impulse_value_key, impulse_value_value in impulse_value.items():
+                                    if col in impulse_value_key:
+                                        # print(impulse_value_value, end='\t')
+                                        all_dict[col] = impulse_value_value
+                        # print()
+                        all_dict_list.append(all_dict)
+        
+                    is_catch_up_saccade_analysis = False
+                    is_impulse = False
+        
+                    meta_dict = {}
+                    impulse_dict = {}
+                    all_dict = {}
+                    impulse_num = 1
+        
+                    key = tokens[0].strip()
+                    value = tokens[1].strip()
+        
+                    meta_dict['Patient Name'] = patient_name
+                    meta_dict['Patient ID'] = patient_id
+                    meta_dict['Patient UID'] = patient_uid
+                    meta_dict[key] = value
+                    
+                    
+                elif 'Test Type' in line:
+                    key = tokens[0].strip()
+                    value = tokens[1].strip()
+        
+                    meta_dict[key] = value
+                    
+                    
+                elif 'Impulse' in line:
+                    if len(impulse_dict.keys()) != 0:
+                        for col in columns:
+                            if col in meta_dict:
+                                # print(meta_dict[col], end='\t')
+                                all_dict[col] = meta_dict[col]
+        
+                            for impulse_key, impulse_value in impulse_dict.items():
+                                for impulse_value_key, impulse_value_value in impulse_value.items():
+                                    if col in impulse_value_key:
+                                        # print(impulse_value_value, end='\t')
+                                        all_dict[col] = impulse_value_value
+                        # print()
+                        impulse_num = impulse_num + 1
+                        all_dict_list.append(all_dict)
+                        
+                    impulse_dict = {}
+                    all_dict = {}
+                    
+                    is_impulse = True
+                    is_catch_up_saccade_analysis = False
+                    
+                    impulse_key = tokens[0].strip()
+                    value_list_key = tokens[1].strip()
+                    value_list_value = tokens[2].strip()
+                    
+                    impulse_dict[impulse_key] = {}
+                    impulse_dict[impulse_key][value_list_key] = value_list_value
+                    
+                    meta_dict['Trial Number'] = str(impulse_num)
+                    impulse_dict[impulse_key] = {}
+                    impulse_dict[impulse_key][value_list_key] = value_list_value
+                    
+                    
+                else:
+                    if 'Catch-up Saccade Analysis' in line:
+                        is_catch_up_saccade_analysis = True
+                        
+                    if len(tokens) == 1:
+                        continue
+                        
+                    if is_catch_up_saccade_analysis:
+                        if tokens[1] and tokens[2]:
+                            key = tokens[1].strip() + ' ' + tokens[2].strip()
+                            s_group = tokens[1].strip()
+                            
+                        elif tokens[2]:
+                            key = s_group + ' ' + tokens[2].strip()
+                            
+                        else:
+                            key = tokens[0].strip() + tokens[1].strip()
+                            
+                        value_left = tokens[-2].strip()
+                        value_right = tokens[-1].strip()
+                        value = (value_left, value_right)
+                        
+                        if key.strip():
+                            meta_dict[key + '-left'] = value_left
+                            meta_dict[key + '-right'] = value_right
+                            
+                        else:
+                            meta_dict[key] = value
+                            
+                    elif is_impulse:
+                        key = tokens[1].strip()
+        
+                        if len(tokens) == 3:
+                            value = tokens[2].strip()
+        
+                        else:
+                            value = tokens[2:]
+        
+                        impulse_dict[impulse_key][key] = value
+        
+                    else:
+                        key = tokens[0].strip()
+                        value = tokens[1].strip()
+        
+                        meta_dict[key] = value
+        
+        
+        with open(result_path, 'w', encoding='UTF-8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=columns, delimiter='\t')
+            writer.writeheader()
+            
+            writer.writerows(all_dict_list)
+
+
 if __name__ == '__main__':
     file_path = 'D:/vHIT/'
     
     for path, dir, files in os.walk(file_path):
         for file in files:
-            if '.xml' in file:
-                xml_path = os.path.join(path, file).replace('\\', '/')
-                input_path = xml_path.strip('.xml') + '.csv'
-                result_path = path.replace('vHIT', 'vHIT_Result') + '/' + file.strip('.xml') + '.tsv'
-                result_dir = os.path.dirname(result_path)
+            try:
+                parse_csv(file)
                 
-                if not os.path.exists(result_dir):
-                    os.makedirs(result_dir)
+            except UnicodeDecodeError:
+                print(file)
                 
-                print(xml_path)
-                print(result_path)
-                
-                is_catch_up_saccade_analysis = False
-                is_impulse = False
-                
-                meta_dict = {}
-                impulse_dict = {}
-                all_dict = {}
-                all_dict_list = []
-                
-                s_group = ''
-                impulse_key = ''
-                impulse_num = 1
-                
-                xml_doc = ET.parse(xml_path)
-                root = xml_doc.getroot()
-                
-                ics_patient = root.findall("{http://tempuri.org/PMRExportDataSet.xsd}ICSPatient")
-                patient_id = [x.findtext("{http://tempuri.org/PMRExportDataSet.xsd}PatientID") for x in ics_patient][0]
-                patient_uid = [x.findtext("{http://tempuri.org/PMRExportDataSet.xsd}PatientUID") for x in ics_patient][0]
-                patient_name = ""
-                
-                columns = get_columns(input_path)
-                print("\t".join(columns))
-                
-                with open(input_path, 'r', encoding='UTF-8') as fin:
-                    for line in fin:
-                        line = line.strip()
-                        tokens = line.split(',')
-                        
-                        if 'Patient Name:' in line:
-                            patient_name = ' '.join(tokens[1:]).strip()
-                            
-                        elif 'Test Date' in line:
-                            if len(meta_dict) != 0:
-                                for col in columns:
-                                    if col in meta_dict:
-                                        print(meta_dict[col], end='\t')
-                                        all_dict[col] = meta_dict[col]
-                
-                                    for impulse_key, impulse_value in impulse_dict.items():
-                                        for impulse_value_key, impulse_value_value in impulse_value.items():
-                                            if col in impulse_value_key:
-                                                print(impulse_value_value, end='\t')
-                                                all_dict[col] = impulse_value_value
-                                print()
-                                all_dict_list.append(all_dict)
-                
-                            is_catch_up_saccade_analysis = False
-                            is_impulse = False
-                
-                            meta_dict = {}
-                            impulse_dict = {}
-                            all_dict = {}
-                            impulse_num = 1
-                
-                            key = tokens[0].strip()
-                            value = tokens[1].strip()
-                
-                            meta_dict['Patient Name'] = patient_name
-                            meta_dict['Patient ID'] = patient_id
-                            meta_dict['Patient UID'] = patient_uid
-                            meta_dict[key] = value
-                
-                        elif 'Test Type' in line:
-                            key = tokens[0].strip()
-                            value = tokens[1].strip()
-                
-                            meta_dict[key] = value
-                
-                        elif 'Impulse' in line:
-                            if len(impulse_dict.keys()) != 0:
-                                for col in columns:
-                                    if col in meta_dict:
-                                        print(meta_dict[col], end='\t')
-                                        all_dict[col] = meta_dict[col]
-                
-                                    for impulse_key, impulse_value in impulse_dict.items():
-                                        for impulse_value_key, impulse_value_value in impulse_value.items():
-                                            if col in impulse_value_key:
-                                                print(impulse_value_value, end='\t')
-                                                all_dict[col] = impulse_value_value
-                                print()
-                                impulse_num = impulse_num + 1
-                                all_dict_list.append(all_dict)
-                                
-                            impulse_dict = {}
-                            all_dict = {}
-                            
-                            is_impulse = True
-                            is_catch_up_saccade_analysis = False
-                            
-                            impulse_key = tokens[0].strip()
-                            value_list_key = tokens[1].strip()
-                            value_list_value = tokens[2].strip()
-                            
-                            impulse_dict[impulse_key] = {}
-                            impulse_dict[impulse_key][value_list_key] = value_list_value
-                            
-                            meta_dict['Trial Number'] = str(impulse_num)
-                            impulse_dict[impulse_key] = {}
-                            impulse_dict[impulse_key][value_list_key] = value_list_value
-                            
-                        else:
-                            if 'Catch-up Saccade Analysis' in line:
-                                is_catch_up_saccade_analysis = True
-                                
-                            if len(tokens) == 1:
-                                continue
-                                
-                            if is_catch_up_saccade_analysis:
-                                if tokens[1] and tokens[2]:
-                                    key = tokens[1].strip() + ' ' + tokens[2].strip()
-                                    s_group = tokens[1].strip()
-                                    
-                                elif tokens[2]:
-                                    key = s_group + ' ' + tokens[2].strip()
-                                    
-                                else:
-                                    key = tokens[0].strip() + tokens[1].strip()
-                                    
-                                value_left = tokens[-2].strip()
-                                value_right = tokens[-1].strip()
-                                value = (value_left, value_right)
-                                
-                                if key.strip():
-                                    meta_dict[key + '-left'] = value_left
-                                    meta_dict[key + '-right'] = value_right
-                                    
-                                else:
-                                    meta_dict[key] = value
-                                    
-                            elif is_impulse:
-                                key = tokens[1].strip()
-                
-                                if len(tokens) == 3:
-                                    value = tokens[2].strip()
-                
-                                else:
-                                    value = tokens[2:]
-                
-                                impulse_dict[impulse_key][key] = value
-                
-                            else:
-                                key = tokens[0].strip()
-                                value = tokens[1].strip()
-                
-                                meta_dict[key] = value
-                
-                
-                with open(result_path, 'w', encoding='UTF-8', newline='') as f:
-                    writer = csv.DictWriter(f, fieldnames=columns, delimiter='\t')
-                    writer.writeheader()
-                    
-                    writer.writerows(all_dict_list)
+            except FileNotFoundError:
+                print(file)
